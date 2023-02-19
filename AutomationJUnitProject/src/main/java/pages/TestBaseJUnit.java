4b41c950805fdc45fd7836536d657a4ce4bdae0d
fazly-github <124215855+fazly-github@users.noreply.github.com>
2023-02-18 22:29:33 -0600

    Add files via upload

---
+++ AutomationJUnitProject/src/main/java/pages/TestBaseJUnit.java
@@ -0,0 +1,25 @@
+package pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBaseJUnit {
+
+	public static WebDriver driver;
+
+	public static WebDriver initDriver() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get("https://techfios.com/test/101/");
+		return driver;
+	}
+
+	public static void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+}
